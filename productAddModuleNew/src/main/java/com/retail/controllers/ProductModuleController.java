3623265e6f373bc79cbd153b4d6ff529034b6d59
LIFETOUCHAD\amoghavarsha <amoghavarsha.contractor@lifetouch.com>
2017-07-21 06:10:03 -0500

    allowed the cross origin requests

--- productAddModuleNew/src/main/java/com/retail/controllers/ProductModuleController.java
+++ productAddModuleNew/src/main/java/com/retail/controllers/ProductModuleController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductModuleController {
 	@RequestMapping(name="MappingName1", path="/productAdd", produces="application/json")
 	public String ProductAddHome() {
-		System.out.println("Inside ProductAddHome!!!");
-		return "Success";
 		
+		return "Success";
 	}
 }
