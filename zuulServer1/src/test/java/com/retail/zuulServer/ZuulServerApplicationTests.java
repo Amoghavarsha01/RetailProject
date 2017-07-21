9a97b2ffcadc8aed3ef6609065045db64dea55b2
LIFETOUCHAD\amoghavarsha <amoghavarsha.contractor@lifetouch.com>
2017-07-21 02:41:59 -0500

    adding zuul proxy

---
+++ zuulServer1/src/test/java/com/retail/zuulServer/ZuulServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.retail.zuulServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class ZuulServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
